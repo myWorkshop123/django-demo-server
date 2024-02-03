fe0fcb0a8f903062249ed453b4652d61179e3ae7
myWorkshop123 <kartikkumar868@gmail.com>
2024-02-04 01:38:47 +0530

    fix | add static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,9 @@ import os
 import environ
 
 env = environ.Env()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
