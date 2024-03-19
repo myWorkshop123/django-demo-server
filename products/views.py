4ef7028d654e4f39030248443310281c0da5eb41
Kartik <kartikkumar868@gmail.com>
2024-03-19 10:03:53 +0530

    test_request

--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from django.http import Http404
 from .models import Product
 from django.http import JsonResponse
-
+import request
 
 def detail(request, poll_id):
     try:
+        base_url = 'https://www.someprivateapi.com/search?'
+        response = request.get(base_url + poll_id)
+        
         p = Product.objects.get(pk=poll_id)
         data = { "id": p.pk, "title": p.title, "description": p.description, "price": p.price}
     except Product.DoesNotExist:
